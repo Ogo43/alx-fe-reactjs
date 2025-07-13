7041b4250b1a1cd58380973376df3e4f2462011f
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-07-13 10:06:58 -0700

    Add More JSX Components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
-
+  const [count, setCount] = useState(0)
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
