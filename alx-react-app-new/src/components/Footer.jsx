efd89aeddb50b70b5a2139bbdbd040333d337334
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-07-20 09:40:05 -0700

    Add new directory and Inline CSS Styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+   return(
+    <footer>
+        <p style={{fontWeight: '300', fontSize: '12px', color: 'whitesmoke'}}>© 2023 City Lovers</p>
+    </footer>
+   ) 
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent () {
+    return (
+        <main>
+            <p style={{fontSize: '1.2rem', color: 'wheat', fontWeight: '700'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent;
