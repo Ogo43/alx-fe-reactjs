2c7bd33298b94c10cab52507c5d662a2e16ccf97
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-07-27 11:41:40 -0700

    Create Recipe App using React and Zustand

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,6 @@
 // RecipeList component
 
-import useRecipeStore from "../store/useRecipeStore";
+import useRecipeStore from "./recipeStore";
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
