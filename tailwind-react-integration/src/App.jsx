5602e9e7ca2b18136a9599f073cadef42ef9e67e
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-08-09 13:35:28 -0700

    Add Tailwindcss styling to the UserProfile

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,12 @@
+import React from "react"
 import "../src/index.css" 
+import UserProfile from "./components/UserProfile"
 
 function App() {
 
   return (
     <>
-      
+      <UserProfile />
     </>
   )
 }
