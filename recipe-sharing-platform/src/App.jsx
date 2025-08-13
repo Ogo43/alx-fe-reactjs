4dabd33da9ce6e4450bda9c864ac802400ff7ab6
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-08-13 12:39:19 -0700

    Create React App using Vite

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-blue-500'>Learning Continue</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
