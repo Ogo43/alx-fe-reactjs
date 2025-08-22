2b921ac1be6ed37708b891cdcacb91b1c1ce3f40
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-08-22 15:45:41 -0700

    Add Jest

---
+++ react-todo/src/TodoList.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addTodo = () => {
+    if (inputValue.trim() === '') return;
+    setTodos([...todos, inputValue]);
+    setInputValue('');
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
--- react-todo/src/_tests_/TodoList.test.js
+++ react-todo/src/_tests_/TodoList.test.js
@@ -1,45 +1,36 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import TodoList from './TodoList';
+import TodoList from '../TodoList';
 
-// eslint-disable-next-line no-undef
 describe('TodoList Component', () => {
-    // eslint-disable-next-line no-undef
-    test('renders TodoList component', () => {
-        render(<TodoList />);
-        // eslint-disable-next-line no-undef
-        expect(screen.getByPlaceholderText(/Add a new todo/i)).toBeInTheDocument();
-        // eslint-disable-next-line no-undef
-        expect(screen.getByText('Add')).toBeInTheDocument();
-    });
+  it('renders input and add button', () => {
+    render(<TodoList />);
+    expect(screen.getByPlaceholderText(/Add a new todo/i)).toBeInTheDocument();
+    expect(screen.getByText('Add')).toBeInTheDocument();
+  });
 
-    // eslint-disable-next-line no-undef
-    test('adds a new todo when Add button is clicked', () => {
-        render(<TodoList />);
-        const input = screen.getByPlaceholderText(/Add a new todo/i);
-        const addButton = screen.getByText('Add');
+  it('adds a new todo when Add button is clicked', () => {
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText(/Add a new todo/i);
+    const addButton = screen.getByText('Add');
 
-        fireEvent.change(input, { target: { value: 'Learn Testing' } });
-        fireEvent.click(addButton);
+    fireEvent.change(input, { target: { value: 'Learn Testing' } });
+    fireEvent.click(addButton);
 
-        // eslint-disable-next-line no-undef
-        expect(screen.getByText('Learn Testing')).toBeInTheDocument();
-    });
+    expect(screen.getByText('Learn Testing')).toBeInTheDocument();
+  });
 
-    // eslint-disable-next-line no-undef
-    test('deletes a todo when Delete button is clicked', () => {
-        render(<TodoList />);
-        const input = screen.getByPlaceholderText(/Add a new todo/i);
-        const addButton = screen.getByText('Add');
+  it('deletes a todo when Delete button is clicked', () => {
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText(/Add a new todo/i);
+    const addButton = screen.getByText('Add');
 
-        fireEvent.change(input, { target: { value: 'Learn Jest' } });
-        fireEvent.click(addButton);
+    fireEvent.change(input, { target: { value: 'Learn Jest' } });
+    fireEvent.click(addButton);
 
-        fireEvent.click(screen.getByText('Delete'));
+    fireEvent.click(screen.getByText('Delete'));
 
-        // eslint-disable-next-line no-undef
-        expect(screen.queryByText('Learn Jest')).not.toBeInTheDocument();
-    });
+    expect(screen.queryByText('Learn Jest')).not.toBeInTheDocument();
+  });
 });
-
-
